f161047cd7848b6999b7f19d6cd585f475052af0
Francisco Diosdado <drz.y2k@gmail.com>
2026-03-31 12:58:13 -0600

    Artículos de Hombre y Mujer

--- src/products/pages/MensPage.tsx
+++ src/products/pages/MensPage.tsx
@@ -1,12 +1,19 @@
-import { ProductList } from ".."
+import { ProductList, useProducts } from '..';
 
 export const MensPage = () => {
-  return (
-    <div className="flex-col">
-      <h1 className="text-2xl font-bold">Productos para hombres</h1>
+	//para las vistas de ropa de hombre y mujer
+	//es la misma funcionalidad, solamente, agregamos el filtro
+	//para que no retorne todos los productos, solo los que queremos filtrar
+	const { isLoading, products } = useProducts({
+		filterKey: "men's clothing",
+	});
+	return (
+		<div className="flex-col">
+			<h1 className="text-2xl font-bold">Productos para hombres</h1>
 
-      <ProductList />
+			{isLoading && <p>Cargando productos...</p>}
 
-    </div>
-  )
-}
+			<ProductList products={products} />
+		</div>
+	);
+};
--- src/products/services/actions.ts
+++ src/products/services/actions.ts
@@ -1,19 +1,33 @@
-import { productsApi } from "../api/productsApi";
+import { productsApi } from '../api/productsApi';
 //importamos como tipo por si ts no lo infiere
-import { type Product } from "../interfaces/product";
+import { type Product } from '../interfaces/product';
 
 interface GetProductsOptions {
 	filterKey?: string;
+}
+
+//sleep para hacer la desmotracion del tiempo de espera de react query
+const sleep = (seconds:number = 0):Promise<boolean> =>{
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(true);
+		}, seconds * 1000);
+	});
 }
 
 //creamos nuestro primer servicio, que consume los productos de nuestra api
 //de momento sin los params, solo para obtener todos los productos, pero vamos definiendo
 //ya el filterkey que usaremos para filtrar nuestros productos
 export const getProducts = async ({ filterKey }: GetProductsOptions) => {
-	const {data} = await productsApi.get<Product[]>("/products", {
-		// params: {
-		// 	filterKey,
-		// },
-	});
+	//agregamos el sleep para que tarde en responder y validar
+	//que toma el caché 
+	await sleep(2);
+
+	//creamos una variable para el filtrado, que agregaremos
+	//dinamicamente a nuestra consulta, ya que así funciona
+	//json server
+	const filterUrl = filterKey ? `category=${filterKey}` : '';
+
+	const { data } = await productsApi.get<Product[]>(`/products?${filterUrl}`);
 	return data;
 };
